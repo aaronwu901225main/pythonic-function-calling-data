6dfc0469d091ed3ea2cdbb7ce7ee18dfa7b4dd23
aaronwu901225main <aaronwu901225main@gmail.com>
2025-10-21 12:30:36 +0800

    新結果

--- berkeley-function-call-leaderboard/bfcl_eval/constants/supported_models.py
+++ berkeley-function-call-leaderboard/bfcl_eval/constants/supported_models.py
@@ -413,21 +413,17 @@ SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-1230-merged-
 SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-1435-merged-1batch-2560seq-3epoch-multi-mix")
 SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-1640-merged-1batch-2560seq-3epoch-multi-mix")
 SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-1842-merged-1batch-2560seq-3epoch-multi-mix")
-
-'''
-mixture-1 的各類型數量統計：
-Simple           :   1290
-Multiple         :   3863
-Parallel         :   1273
-Parallel-Multiple:   4074
-Revalance  :   3500
-以上來自apigen
----------------------------------
-multi-zhtw        :   3000(目前使用的量) 
-multi-eng        :   3000(目前使用的量) 
----------------------------------
-'''
-
+#mixture-1 的各類型數量統計：
+#Simple           :   1290
+#Multiple         :   3863
+#Parallel         :   1273
+#Parallel-Multiple:   4074
+#Revalance  :   3500
+#以上來自apigen
+#---------------------------------
+#multi-zhtw        :   3000(目前使用的量) 
+#multi-eng        :   3000(目前使用的量) 
+#---------------------------------
 # 1batch-2560seq-3epoch-accumulate64-mixture-1
 SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-102-merged-1batch-2560seq-3epoch-accumulate64-mixture-1")
 SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-204-merged-1batch-2560seq-3epoch-accumulate64-mixture-1")
@@ -452,20 +448,17 @@ SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-459-merged-1
 SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-510-merged-1batch-2560seq-4epoch-accumulate128-mixture-1")
 SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-561-merged-1batch-2560seq-4epoch-accumulate128-mixture-1")
 SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-612-merged-1batch-2560seq-4epoch-accumulate128-mixture-1")
-'''
-mixture-2 的各類型數量統計：
-Simple           :   1290
-Multiple         :   3863
-Parallel         :   1273
-Parallel-Multiple:   4074
-Revalance  :   1500
-以上來自apigen
----------------------------------
-multi-zhtw        :   2500(目前使用的量) 
-multi-eng        :   2500(目前使用的量) 
----------------------------------
-'''
-
+#mixture-2 的各類型數量統計：
+#Simple           :   1290
+#Multiple         :   3863
+#Parallel         :   1273
+#Parallel-Multiple:   4074
+#Revalance  :   1500
+#以上來自apigen
+#---------------------------------
+#multi-zhtw        :   2500(目前使用的量) 
+#multi-eng        :   2500(目前使用的量) 
+#---------------------------------
 # 1batch-2560seq-3epoch-accumulate32-mixture-2
 SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-177-merged-1batch-2560seq-3epoch-accumulate32-mixture-2")
 SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-354-merged-1batch-2560seq-3epoch-accumulate32-mixture-2")
