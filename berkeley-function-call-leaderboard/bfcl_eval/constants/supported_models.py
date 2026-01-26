c1ba83dde9e58b6f3cd58d138a454d7ac9140c55
aaronwu901225main <aaronwu901225main@gmail.com>
2026-01-27 00:19:52 +0800

    New results

--- berkeley-function-call-leaderboard/bfcl_eval/constants/supported_models.py
+++ berkeley-function-call-leaderboard/bfcl_eval/constants/supported_models.py
@@ -763,7 +763,10 @@ SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-1448-merged-
 SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-1600-merged-1epoch-all-zhtw-lr5e-7")
 SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-3200-merged-1epoch-all-zhtw-lr5e-7")
 SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-4787-merged-1epoch-all-zhtw-lr5e-7")
-
+# 3epoch-all-zhtw-lr5e-7
+SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-1550-merged-3epoch-all-zhtw-lr5e-7")
+SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-7750-merged-3epoch-all-zhtw-lr5e-7")
+SUPPORTED_MODELS.append("Salesforce/Llama-xLAM-2-8b-fc-r-checkpoint-14361-merged-3epoch-all-zhtw-lr5e-7")
 
 # Qwen/Qwen2.5-7B-Instruct 
 # Base
